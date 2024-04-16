a94c77b1b1ff36e5deb941c4c7d5e9740a88cc39
b0ink <40929320+b0ink@users.noreply.github.com>
2024-04-16 15:31:16 +1000

    Remove dummy data

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-
         List<NewsItem> newsList = JsonHelper.parseArticles(this);
         List<NewsItem> topNewsList = JsonHelper.parseArticles(this);
 
@@ -40,31 +39,11 @@ public class MainActivity extends AppCompatActivity {
         NewsAdapter topNewsAdapter = new NewsAdapter(topNewsList, true);
         topNewsRecycler.setAdapter(topNewsAdapter);
 
-
         /* Standard news articles */
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
         NewsAdapter adapter = new NewsAdapter(newsList, false);
         recyclerView.setAdapter(adapter);
 
-
-    }
-
-    private List<NewsItem> generateDummyData() {
-        List<NewsItem> newsList = new ArrayList<>();
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 16, 2023", "Sample Headline 1"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 15, 2022", "Sample Headline 2"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 14, 2021", "Sample Headline 3"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 13, 2024", "Sample Headline 4"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 12, 2025", "Sample Headline 5"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 6"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 7"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 8"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 9"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 10"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 11"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 12"));
-        newsList.add(new NewsItem(R.drawable.placeholder_image, "April 11, 2026", "Sample Headline 13"));
-        return newsList;
     }
 }
