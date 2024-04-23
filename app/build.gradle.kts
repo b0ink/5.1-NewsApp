80d93a0553fdf3791535a9d9351588d06200856e
b0ink <40929320+b0ink@users.noreply.github.com>
2024-04-23 12:55:01 +1000

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
